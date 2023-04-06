3e6a8fd28bb6a26c473d214c190289d009a0e98e
Vlad <vlad.khvesyukovich@gmail.com>
2023-04-06 12:56:32 +0300

    t9 done

---
+++ code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
